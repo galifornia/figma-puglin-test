8e0c148bc6c06ca3ad99d7e68d98b6891c40c5c8
galifornia <martinho87@gmail.com>
2021-04-28 19:44:44 +0200

    Select all objects with same name as the one selected

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
-figma.closePlugin()
+function isSameName(node) {
+  let savedName = figma.currentPage.selection[0].name;
+  return node.name === savedName;
+}
+
+function main() {
+  if (figma.currentPage.selection.length !== 1) {
+    figma.closePlugin("Select only one object")
+    return;
+  }
+
+  let nodes = figma.currentPage.findAll(isSameName);
+  figma.currentPage.selection = nodes;
+
+  figma.closePlugin("Objects selected")
+}
+
+main();
